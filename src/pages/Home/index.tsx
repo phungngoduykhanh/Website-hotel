e8ae995df707d480bb5a3937d51b316ff060b1b7
Khanh <khanhabcdd1@gmail.com>
2023-07-13 16:06:33 +0700

    first commit

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../../layouts/Header'
+import Footer from '../../layouts/Footer'
+
+export default function Home() {
+  return (
+    <div>
+        <Header/>
+        contain
+        <Footer/>
+    </div>
+  )
+}
