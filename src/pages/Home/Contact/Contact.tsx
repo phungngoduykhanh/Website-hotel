3a52e1da6049eb9c3c98d514509b78e3fcad169a
KieuMei2811 <hothikieu28112003@gmail.com>
2023-07-18 11:07:24 +0700

    update-Contact

--- src/pages/Home/Contact/Contact.tsx
+++ src/pages/Home/Contact/Contact.tsx
@@ -1,10 +1,11 @@
-import React from "react";
-import { Container, Row, Col, Image, Button } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Container, Row, Col, Image, Button, Carousel } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLocationDot, faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import "bootstrap/dist/css/bootstrap.min.css";
 import './Contact.css';
 export default function Contact() {
+
     return (
         <>
             <div className="content">
@@ -50,35 +51,72 @@ export default function Contact() {
                     <Row>
                         <h1 className="content_people-say">People Says</h1>
                     </Row><br /><br />
-                    <Row className="content_people-infor">
-                        <Col xs={12} md={9}>
-                            <Image className="content_people-img" src="https://themewagon.github.io/sogo/images/person_1.jpg" roundedCircle />
-                            <p className="content_people-text">“A small river named Duden flows by their place and supplies it with the necessary regelialia.
-                                It is a paradisematic country, in which roasted parts of sentences fly into your mouth.”
-                            </p>
-                            <p className="content_people-text">
-                                -- Kieu
-                            </p>
-                        </Col>
-                        <Col xs={12} md={9}>
-                            <Image className="content_people-img" src="https://themewagon.github.io/sogo/images/person_2.jpg" roundedCircle /> <br/>
-                            <p className="content_people-text">“A small river named Duden flows by their place and supplies it with the necessary regelialia.
-                                It is a paradisematic country, in which roasted parts of sentences fly into your mouth.”
-                            </p>
-                            <p className="content_people-text">
-                                -- Kieu
-                            </p>
-                        </Col>
-                        <Col xs={12} md={9}>
-                            <Image className="content_people-img" src="https://themewagon.github.io/sogo/images/person_3.jpg" roundedCircle />
-                            <p className="content_people-text">“A small river named Duden flows by their place and supplies it with the necessary regelialia.
-                                It is a paradisematic country, in which roasted parts of sentences fly into your mouth.”
-                            </p>
-                            <p className="content_people-text">
-                                -- Kieu
-                            </p>
-                        </Col>
-                    </Row>
+                    {/* <Row className="content_people-infor">                       */}
+                    <Carousel >
+                        <Carousel.Item>
+                            <Row >
+                                <Col md={4} className='d-flex justify-content-center  align-items-center flex-column'>
+                                    <Image className="content_people-img" src="https://themewagon.github.io/sogo/images/person_1.jpg" roundedCircle /> <br />
+                                    <p className="content_people-text">“A small river named Duden flows by their place and supplies it with the necessary regelialia.
+                                        It is a paradisematic country, in which roasted parts of sentences fly into your mouth.”
+                                    </p>
+                                    <p className="content_people-text">
+                                        -- Kieu
+                                    </p>
+                                </Col>
+                                <Col md={4} className='d-flex justify-content-center  align-items-center flex-column'>
+                                    <Image className="content_people-img" src="https://themewagon.github.io/sogo/images/person_2.jpg" roundedCircle /> <br />
+                                    <p className="content_people-text">“A small river named Duden flows by their place and supplies it with the necessary regelialia.
+                                        It is a paradisematic country, in which roasted parts of sentences fly into your mouth.”
+                                    </p>
+                                    <p className="content_people-text">
+                                        -- Kieu
+                                    </p>
+                                </Col>
+                                <Col md={4} className='d-flex justify-content-center  align-items-center flex-column'>
+                                    <Image className="content_people-img" src="https://themewagon.github.io/sogo/images/person_3.jpg" roundedCircle /> <br />
+                                    <p className="content_people-text">“A small river named Duden flows by their place and supplies it with the necessary regelialia.
+                                        It is a paradisematic country, in which roasted parts of sentences fly into your mouth.”
+                                    </p>
+                                    <p className="content_people-text">
+                                        -- Kieu
+                                    </p>
+                                </Col>
+                            </Row>
+                        </Carousel.Item>
+                        <Carousel.Item >
+                            <Row >
+                                <Col md={4} className='d-flex justify-content-center  align-items-center flex-column'>
+                                    <Image className="content_people-img" src="https://themewagon.github.io/sogo/images/person_1.jpg" roundedCircle /> <br />
+                                    <p className="content_people-text">“A small river named Duden flows by their place and supplies it with the necessary regelialia.
+                                        It is a paradisematic country, in which roasted parts of sentences fly into your mouth.”
+                                    </p>
+                                    <p className="content_people-text">
+                                        -- Kieu
+                                    </p>
+                                </Col>
+                                <Col md={4} className='d-flex justify-content-center  align-items-center flex-column'>
+                                    <Image className="content_people-img" src="https://themewagon.github.io/sogo/images/person_2.jpg" roundedCircle /> <br />
+                                    <p className="content_people-text">“A small river named Duden flows by their place and supplies it with the necessary regelialia.
+                                        It is a paradisematic country, in which roasted parts of sentences fly into your mouth.”
+                                    </p>
+                                    <p className="content_people-text">
+                                        -- Kieu
+                                    </p>
+                                </Col>
+                                <Col md={4} className='d-flex justify-content-center  align-items-center flex-column'>
+                                    <Image className="content_people-img" src="https://themewagon.github.io/sogo/images/person_4.jpg" roundedCircle /> <br />
+                                    <p className="content_people-text">“A small river named Duden flows by their place and supplies it with the necessary regelialia.
+                                        It is a paradisematic country, in which roasted parts of sentences fly into your mouth.”
+                                    </p>
+                                    <p className="content_people-text">
+                                        -- Kieu
+                                    </p>
+                                </Col>
+                            </Row>
+                        </Carousel.Item>
+                    </Carousel>
+                    {/* </Row> */}
                 </Container>
             </div>
         </>
