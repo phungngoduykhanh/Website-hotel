e246919b80b9ff150cdef98343211c3ace697824
ThangNH23 <huuthang533540403@gmail.com>
2023-08-04 10:21:56 +0700

    edit responsive

--- src/pages/Detail/Detail.tsx
+++ src/pages/Detail/Detail.tsx
@@ -20,30 +20,33 @@ const Detail: React.FC = () => {
       setActiveTab(eventKey);
     }
   };
-  
+
   const [documents] = useAllPrismicDocumentsByType("hotelroom");
 
   const [hotelPLC] = useSinglePrismicDocument("hotel");
 
-  const [doc] = useAllPrismicDocumentsByType('gallery');
+  const [doc] = useAllPrismicDocumentsByType("gallery");
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const [photoIndex, setPhotoIndex] = useState<number>(0);
-  
+
   function openLightbox(index: number): void {
     setPhotoIndex(index);
     setIsOpen(true);
-}
+  }
 
-const images: string[] = doc?.flatMap((doc) => {
-  return doc.data.body[0].items.map((item: { link_image: { url: any; }; }) => {
-    return item.link_image?.url;
-  });
-}) || [];
+  const images: string[] =
+    doc?.flatMap((doc) => {
+      return doc.data.body[0].items.map(
+        (item: { link_image: { url: any } }) => {
+          return item.link_image?.url;
+        }
+      );
+    }) || [];
 
-  if (!documents || documents.length === 0 ) {
+  if (!documents || documents.length === 0) {
     return null; // Return early if any of the data is not available or loading
   }
- 
+
   const renderContent = () => {
     if (activeTab === "home") {
       return (
@@ -58,7 +61,7 @@ const images: string[] = doc?.flatMap((doc) => {
             {documents.map((docs, index) => {
               const { data } = docs;
               return (
-                <div key={index} className="col-md-4 mb-4 my-4">
+                <div key={index} className="col-lg-4 col-md-6 col-sm-6 mb-4 my-4">
                   <Link
                     to={`/detailroom/${docs.uid}`}
                     className="text-decoration-none text-dark"
@@ -69,16 +72,16 @@ const images: string[] = doc?.flatMap((doc) => {
                         alt=""
                         className="w-100 img-fluid"
                         style={{
+                          height:"250px",
                           objectFit: "cover",
-                          height: "250px",
-                          maxHeight: "250px",
+                          maxHeight: "350px",
                           maxWidth: "250px",
+                          margin:"0 auto",
                         }}
                       />
                       <h4
-                        className="pt-3 mb-1"
+                        className="pt-3 mb-1 text-center"
                         style={{
-                          textAlign: "center",
                           textShadow: "1px 0 1px #080808",
                           // fontFamily: "Lora, serif",
                         }}
@@ -86,8 +89,8 @@ const images: string[] = doc?.flatMap((doc) => {
                         {data.name_room[0].text}
                       </h4>
                       <p
-                        className=" mb-1 pt-2 "
-                        style={{ letterSpacing: "3px", textAlign: "center" }}
+                        className="mb-1 pt-2 text-center"
+                        style={{ letterSpacing: "3px" }}
                       >
                         {data.people[0].text}
                       </p>
@@ -105,7 +108,7 @@ const images: string[] = doc?.flatMap((doc) => {
           <div className="row my-4">
             <div className="col-md-1"></div>
             <div className="col-md-10">
-            <div className="row">
+              <div className="row">
                 {images.map((image: string, index: number) => (
                   <div className="col-md-3 image-wrapper-detail p-3">
                     <img
@@ -115,20 +118,31 @@ const images: string[] = doc?.flatMap((doc) => {
                       className="w-100 images-detail "
                       style={{ objectFit: "cover", height: "250px" }}
                     />
-                    <div className="overlay-roomdetail" onClick={() => openLightbox(index)}></div>
+                    <div
+                      className="overlay-roomdetail"
+                      onClick={() => openLightbox(index)}
+                    ></div>
                   </div>
                 ))}
                 {isOpen && (
-                    <Lightbox
-                        mainSrc={images[photoIndex]}
-                        nextSrc={images[(photoIndex + 1) % images.length]}
-                        prevSrc={images[(photoIndex + images.length - 1) % images.length]}
-                        onCloseRequest={() => setIsOpen(false)}
-                        onMovePrevRequest={() => setPhotoIndex((photoIndex + images.length - 1) % images.length)}
-                        onMoveNextRequest={() => setPhotoIndex((photoIndex + 1) % images.length)}
-                    />
+                  <Lightbox
+                    mainSrc={images[photoIndex]}
+                    nextSrc={images[(photoIndex + 1) % images.length]}
+                    prevSrc={
+                      images[(photoIndex + images.length - 1) % images.length]
+                    }
+                    onCloseRequest={() => setIsOpen(false)}
+                    onMovePrevRequest={() =>
+                      setPhotoIndex(
+                        (photoIndex + images.length - 1) % images.length
+                      )
+                    }
+                    onMoveNextRequest={() =>
+                      setPhotoIndex((photoIndex + 1) % images.length)
+                    }
+                  />
                 )}
-                </div>
+              </div>
             </div>
             <div className="col-md-1"></div>
           </div>
@@ -137,57 +151,61 @@ const images: string[] = doc?.flatMap((doc) => {
     } else if (activeTab === "link-3") {
       return (
         <div className="container">
-            <div className="row">
-              <div className="col-md-1"></div>
-              <div className="col-md-10 custom-margin mt-1 mb-3 ">
-                <h3 className="pt-3 my-3">Amenities</h3>
-                <div className="row">
-                  {hotelPLC &&
-                    hotelPLC.data &&
-                    hotelPLC.data.body &&
-                    hotelPLC.data.body[0].items.map(
-                      (
-                        item: { amenityname: RichTextField | null | undefined },
-                        index: React.Key | null | undefined
-                      ) => (
-                        <div className="col-md-3" key={index}>
-                          <ul>
-                            <li
-                              style={{
-                                paddingTop: "5px",
-                              }}
-                            >
-                              <PrismicRichText field={item.amenityname} />
-                            </li>
-                          </ul>
-                        </div>
-                      )
-                    )}
-                </div>
-              </div>
-              <div className="col-md-1"></div>
-            </div>
-            <div className="row">
-              <div className="col-md-1"></div>
-              <div className="col-md-10">
-                <h3 className="pt-4">Policies</h3> <br />
-                <b>General Policy <hr /></b>
-                <p>
-                  {hotelPLC && (
-                    <PrismicRichText field={hotelPLC.data.hotelpolicy} />
+          <div className="row">
+            <div className="col-md-1"></div>
+            <div className="col-md-10 custom-margin mt-1 mb-3 ">
+              <h3 className="pt-3 my-3">Amenities</h3>
+              <div className="row">
+                {hotelPLC &&
+                  hotelPLC.data &&
+                  hotelPLC.data.body &&
+                  hotelPLC.data.body[0].items.map(
+                    (
+                      item: { amenityname: RichTextField | null | undefined },
+                      index: React.Key | null | undefined
+                    ) => (
+                      <div className="col-md-3" key={index}>
+                        <ul>
+                          <li
+                            style={{
+                              paddingTop: "5px",
+                            }}
+                          >
+                            <PrismicRichText field={item.amenityname} />
+                          </li>
+                        </ul>
+                      </div>
+                    )
                   )}
-                </p>
-                <br />
-                <b>Payment policy <hr /></b>
-                <p> Please pay the deposit to the account: </p>
-                <p>
-                  {hotelPLC && (
-                    <PrismicRichText field={hotelPLC.data.hotelpayment} />
-                  )}
-                </p>
               </div>
-              <div className="col-md-1"></div>
             </div>
+            <div className="col-md-1"></div>
+          </div>
+          <div className="row">
+            <div className="col-md-1"></div>
+            <div className="col-md-10">
+              <h3 className="pt-4">Policies</h3> <br />
+              <b>
+                General Policy <hr />
+              </b>
+              <p>
+                {hotelPLC && (
+                  <PrismicRichText field={hotelPLC.data.hotelpolicy} />
+                )}
+              </p>
+              <br />
+              <b>
+                Payment policy <hr />
+              </b>
+              <p> Please pay the deposit to the account: </p>
+              <p>
+                {hotelPLC && (
+                  <PrismicRichText field={hotelPLC.data.hotelpayment} />
+                )}
+              </p>
+            </div>
+            <div className="col-md-1"></div>
+          </div>
         </div>
       );
     }
@@ -223,7 +241,7 @@ const images: string[] = doc?.flatMap((doc) => {
                 marginTop: "3rem",
                 gap: "1rem",
                 paddingLeft: "5.8rem",
-                fontWeight:"bold",
+                fontWeight: "bold",
                 color: "black",
               }}
               variant="tabs"
