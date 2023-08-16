7e7f5fade6d73f2f6a976035a71290bc558edb59
Khanh <khanhabcdd1@gmail.com>
2023-08-16 10:04:25 +0700

    update form-input

--- src/pages/Home/Session1.tsx
+++ src/pages/Home/Session1.tsx
@@ -3,96 +3,95 @@ import React from 'react'
 export default function Session1() {
   return (
     <section className="section bg-light pb-0 style-input-home">
-  <div className="container">
-    <div className="row check-availabilty" id="next">
-      <div className="block-32" data-aos="fade-up">
-        <form action="#">
-          <div className="row">
-            <div className="col-md-6 mb-3 mb-lg-0 col-lg-3">
-              <label
-                htmlFor="checkin_date"
-                className="font-weight-bold text-black"
-              >
-                Check In
-              </label>
-              <div className="field-icon-wrap">
-                <div className="icon">
-                  <span className="icon-calendar" />
-                </div>
-                <input type="text" id="checkin_date" className="form-control" />
-              </div>
-            </div>
-            <div className="col-md-6 mb-3 mb-lg-0 col-lg-3">
-              <label
-                htmlFor="checkout_date"
-                className="font-weight-bold text-black"
-              >
-                Check Out
-              </label>
-              <div className="field-icon-wrap">
-                <div className="icon">
-                  <span className="icon-calendar" />
-                </div>
-                <input
-                  type="text"
-                  id="checkout_date"
-                  className="form-control"
-                />
-              </div>
-            </div>
-            <div className="col-md-6 mb-3 mb-md-0 col-lg-3">
+      <div className="container">
+        <div className="row check-availabilty" id="next">
+          <div className="block-32" data-aos="fade-up">
+            <form action="#">
               <div className="row">
-                <div className="col-md-6 mb-3 mb-md-0">
+                <div className="col-md-6 mb-3 mb-lg-0 col-lg-3">
                   <label
-                    htmlFor="adults"
                     className="font-weight-bold text-black"
                   >
-                    Adults
+                    Check In
                   </label>
                   <div className="field-icon-wrap">
-                      <div className="icon">
-                      <span className="ion-ios-arrow-down" />
+                    <div className="icon">
+                      <span className="icon-calendar" />
                     </div>
-                    <select name="" id="adults" className="form-control">
-                      <option value="">1</option>
-                      <option value="">2</option>
-                      <option value="">3</option>
-                      <option value="">4+</option>
-                    </select>
+                    <input type="date" id="checkin_date" className="form-control" />
                   </div>
                 </div>
-                <div className="col-md-6 mb-3 mb-md-0">
+                <div className="col-md-6 mb-3 mb-lg-0 col-lg-3">
                   <label
-                    htmlFor="children"
+                    htmlFor="checkout_date"
                     className="font-weight-bold text-black"
                   >
-                    Children
+                    Check Out
                   </label>
-                  <div className="field-icon-wrap">  
-                  
+                  <div className="field-icon-wrap">
                     <div className="icon">
-                      <span className="ion-ios-arrow-down" />
+                      <span className="icon-calendar" />
                     </div>
-                    <select name="" id="children" className="form-control">
-                      <option value="">1</option>
-                      <option value="">2</option>
-                      <option value="">3</option>
-                      <option value="">4+</option>
-                    </select>
+                    <input
+                      type="date"
+                      id="checkout_date"
+                      className="form-control"
+                    />
                   </div>
                 </div>
+                <div className="col-md-6 mb-3 mb-md-0 col-lg-3">
+                  <div className="row">
+                    <div className="col-md-6 mb-3 mb-md-0">
+                      <label
+                        htmlFor="adults"
+                        className="font-weight-bold text-black"
+                      >
+                        Adults
+                      </label>
+                      <div className="field-icon-wrap">
+                        <div className="icon">
+                          <span className="ion-ios-arrow-down" />
+                        </div>
+                        <select name="" id="adults" className="form-control">
+                          <option value="">1</option>
+                          <option value="">2</option>
+                          <option value="">3</option>
+                          <option value="">4+</option>
+                        </select>
+                      </div>
+                    </div>
+                    <div className="col-md-6 mb-3 mb-md-0">
+                      <label
+                        htmlFor="children"
+                        className="font-weight-bold text-black"
+                      >
+                        Children
+                      </label>
+                      <div className="field-icon-wrap">
+
+                        <div className="icon">
+                          <span className="ion-ios-arrow-down" />
+                        </div>
+                        <select name="" id="children" className="form-control">
+                          <option value="">1</option>
+                          <option value="">2</option>
+                          <option value="">3</option>
+                          <option value="">4+</option>
+                        </select>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-md-6 col-lg-3 align-self-end" >
+                  <button className="btn btn-primary btn-block text-white style-button-home">
+                    Check Availabilty
+                  </button>
+                </div>
               </div>
-            </div>
-            <div className="col-md-6 col-lg-3 align-self-end" >
-              <button className="btn btn-primary btn-block text-white style-button-home">
-                Check Availabilty
-              </button> 
-            </div>
+            </form>
           </div>
-        </form>
+        </div>
       </div>
-    </div>
-  </div>
-</section>
+    </section>
   )
 }
