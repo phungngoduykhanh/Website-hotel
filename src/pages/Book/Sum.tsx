6a23aa5ca79b994e686fa634a1b106ba22fa663e
KieuMei2811 <hothikieu28112003@gmail.com>
2023-08-24 15:50:45 +0700

    update sum

--- src/pages/Book/Sum.tsx
+++ src/pages/Book/Sum.tsx
@@ -1,67 +1,71 @@
 import React from "react";
 import Form from "react-bootstrap/Form";
 import "./Sum.css";
+import { useLocation } from "react-router-dom";
 
 export default function Sum() {
-  return (
-    <div className="summary ">
-      <div className="booking"
-        style={{ border: "1px solid", width: "13rem", borderRadius: "10px", backgroundColor:"#f5f6fa", color:"black" }}>
+	const location = useLocation();
+	const { checkIn, checkOut, people: peopleCount} = location.state;
 
-        <div className="s pt-2"
-          style={{ fontWeight: "bold", textAlign: "center" }}>
-          Your booking summary
-        </div>
-        <hr style={{ margin: "10px 10px" }} />
-        <Form.Text id="text" muted>
-          <div className="row d-column pl-3 pr-3" style={{color:"black"}}>
-            <div className="col-lg-12">
-              <label className="pull-left" style={{fontWeight:"bold"}}>check in:</label>
-              <label className="pull-right">
-                <span className="text-form" style={{color:"orange"}}>14/08/2023</span>
-              </label>
-            </div>
-            <div className="col-lg-12">
-              <label className="pull-left" style={{fontWeight:"bold"}}>check out:</label>
-              <label className="pull-right">
-                <span className="text-form" style={{color:"orange"}}>15/08/2023</span>
-              </label>
-            </div>
-            <div className="col-lg-12">
-              <label className="pull-left" style={{fontWeight:"bold"}}>Number of people:</label>
-              <label className="pull-right">
-                <span className="text-form" style={{color:"orange"}}>2</span>
-              </label>
-            </div>
-            <div className="col-lg-12">
-              <label className="pull-left" style={{fontWeight:"bold"}}>Number of Room (s):</label>
-              <label className="pull-right">
-                <span className="text-form" style={{color:"orange"}}>2</span>
-              </label>
-            </div>
-          </div>
-        </Form.Text>
+	return (
+		<div className="summary ">
+			<div className="booking" 
+				style={{ border: "1px solid", width: "13rem", borderRadius: "10px", backgroundColor: "#f5f6fa", color: "black" }}>
 
-        <hr style={{ margin: "14px 10px" }} />
+				<div className="s pt-2"
+					style={{ fontWeight: "bold", textAlign: "center" }}>
+					Your booking summary
+				</div>
+				<hr style={{ margin: "10px 10px" }} />
+				<Form.Text id="text" muted>
+					<div className="row d-column pl-3 pr-3" style={{ color: "black" }}>
+						<div className="col-lg-12">
+							<label className="pull-left" style={{ fontWeight: "bold" }}>check in:</label>
+							<label className="pull-right">
+								<span className="text-form" style={{ color: "orange" }}>{checkIn}</span>
+							</label>
+						</div>
+						<div className="col-lg-12">
+							<label className="pull-left" style={{ fontWeight: "bold" }}>check out:</label>
+							<label className="pull-right">
+								<span className="text-form" style={{ color: "orange" }}>{checkOut}</span>
+							</label>
+						</div>
+						<div className="col-lg-12">
+							<label className="pull-left" style={{ fontWeight: "bold" }}>Number of people:</label>
+							<label className="pull-right">
+								<span className="text-form" style={{ color: "orange" }}>{peopleCount}</span>
+							</label>
+						</div>
+						<div className="col-lg-12">
+							<label className="pull-left" style={{ fontWeight: "bold" }}>Number of Room (s):</label>
+							<label className="pull-right">
+								<span className="text-form" style={{ color: "orange" }}>2</span>
+							</label>
+						</div>
+					</div>
+				</Form.Text>
 
-        <Form.Text id="text" muted>
-          <div className="row d-column" style={{color:"black"}}>
-            <div className="col-lg-12">
-              {/* <div className="" style={{fontWeight:"bold"}}>Double Room (x1)</div> */}
-              <label className="">
-                <span className="text-form" style={{color:"orange"}}></span>
-              </label>
-            </div>
-          </div>
-        </Form.Text>
-        <a href="#" className="conti">
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          Continues
-        </a>
-      </div>
-    </div>
-  );
+				<hr style={{ margin: "14px 10px" }} />
+
+				<Form.Text id="text" muted>
+					<div className="row d-column" style={{ color: "black" }}>
+						<div className="col-lg-12">
+							{/* <div className="" style={{fontWeight:"bold"}}>Double Room (x1)</div> */}
+							<label className="">
+								<span className="text-form" style={{ color: "orange" }}></span>
+							</label>
+						</div>
+					</div>
+				</Form.Text>
+				<a href="#" className="conti">
+					<span></span>
+					<span></span>
+					<span></span>
+					<span></span>
+					Continues
+				</a>
+			</div>
+		</div>
+	);
 }
--- src/pages/Home/Session1.tsx
+++ src/pages/Home/Session1.tsx
@@ -1,100 +1,131 @@
-import React from 'react'
-import { useLocation } from 'react-router-dom';
+import React, { ChangeEvent, useState } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom';
 import './home.css';
 export default function Session1() {
-  const location = useLocation();
-  const session1ClassName = location.pathname === '/booking';
-  return (
-    <section className={`section pb-0 style-input-home ${session1ClassName ? "" : "bg-light"}`}>
-      <div className="container">
-        <div className="row check-availabilty" id="next">
-          <div className="block-32" data-aos="fade-up">
-            <form action="#">
-              <div className="row">
-                <div className="col-md-6 mb-3 mb-lg-0 col-lg-3">
-                  <label
-                    className="font-weight-bold text-black"
-                  >
-                    Check In
-                  </label>
-                  <div className="field-icon-wrap">
-                    <div className="icon">
-                      <span className="icon-calendar" />
-                    </div>
-                    <input type="date" id="checkin_date" className="form-control" />
-                  </div>
-                </div>
-                <div className="col-md-6 mb-3 mb-lg-0 col-lg-3">
-                  <label
-                    htmlFor="checkout_date"
-                    className="font-weight-bold text-black"
-                  >
-                    Check Out
-                  </label>
-                  <div className="field-icon-wrap">
-                    <div className="icon">
-                      <span className="icon-calendar" />
-                    </div>
-                    <input
-                      type="date"
-                      id="checkout_date"
-                      className="form-control"
-                    />
-                  </div>
-                </div>
-                <div className="col-md-6 mb-3 mb-md-0 col-lg-3">
-                  <div className="row">
-                    <div className="col-md-6 mb-3 mb-md-0">
-                      <label
-                        htmlFor="adults"
-                        className="font-weight-bold text-black"
-                      >
-                        Adults
-                      </label>
-                      <div className="field-icon-wrap">
-                        <div className="icon">
-                          <span className="ion-ios-arrow-down" />
-                        </div>
-                        <select name="" id="adults" className="form-control">
-                          <option value="">1</option>
-                          <option value="">2</option>
-                          <option value="">3</option>
-                          <option value="">4+</option>
-                        </select>
-                      </div>
-                    </div>
-                    <div className="col-md-6 mb-3 mb-md-0">
-                      <label
-                        htmlFor="children"
-                        className="font-weight-bold text-black"
-                      >
-                        Children
-                      </label>
-                      <div className="field-icon-wrap">
+	const [checkIn, setCheckIn] = useState<string>("");
+	const [checkOut, setCheckOut] = useState<string>("");
+	const [adult, setAdult] = useState<number | null>(null);
+	const [children, setChildren] = useState<number | null>(null);
+	const location = useLocation();
+	const session1ClassName = location.pathname === '/booking';
 
-                        <div className="icon">
-                          <span className="ion-ios-arrow-down" />
-                        </div>
-                        <select name="" id="children" className="form-control">
-                          <option value="">1</option>
-                          <option value="">2</option>
-                          <option value="">3</option>
-                          <option value="">4+</option>
-                        </select>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-md-6 col-lg-3 align-self-end" >
-                  <button className="btn btn-primary btn-block text-white style-button-home">
-                    Check Availabilty
-                  </button>
-                </div>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-    </section>
-  )
+	const handleChange = (event: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+		const { name, value } = event.target;
+
+		if (name === "checkIn") {
+			setCheckIn(value);
+		} else if (name === "checkOut") {
+			setCheckOut(value);
+		} else if (name === "adult") {
+			setAdult(parseInt(value));
+		} else if (name === "children") {
+			setChildren(parseInt(value));
+		}
+	};
+
+	const navigate = useNavigate();
+
+	const Show = () => {
+		const totalPeople = (adult: number | null, children: number | null): number => {
+			const adultCount = adult || 0;
+			const childrenCount = children || 0;
+			return adultCount + childrenCount;
+		};
+
+		const peopleCount = totalPeople(adult, children);
+
+		navigate('/booking', { state: { checkIn, checkOut, people: peopleCount } });
+	}
+
+	return (
+		<section className={`section pb-0 style-input-home ${session1ClassName ? "" : "bg-light"}`}>
+			<div className="container">
+				<div className="row check-availabilty" id="next">
+					<div className="block-32" data-aos="fade-up">
+						<form action="#">
+							<div className="row">
+								<div className="col-md-6 mb-3 mb-lg-0 col-lg-3">
+									<label
+										className="font-weight-bold text-black"
+									>
+										Check In
+									</label>
+									<div className="field-icon-wrap">
+										<div className="icon">
+											<span className="icon-calendar" />
+										</div>
+										<input type="date" id="checkin_date" className="form-control" name="checkIn" onChange={handleChange} value={checkIn} />
+									</div>
+								</div>
+								<div className="col-md-6 mb-3 mb-lg-0 col-lg-3">
+									<label
+										htmlFor="checkout_date"
+										className="font-weight-bold text-black"
+									>
+										Check Out
+									</label>
+									<div className="field-icon-wrap">
+										<div className="icon">
+											<span className="icon-calendar" />
+										</div>
+										<input type="date" id="checkout_date" className="form-control" name="checkOut" onChange={handleChange} value={checkOut} />
+									</div>
+								</div>
+								<div className="col-md-6 mb-3 mb-md-0 col-lg-3">
+									<div className="row">
+										<div className="col-md-6 mb-3 mb-md-0">
+											<label
+												htmlFor="adults"
+												className="font-weight-bold text-black"
+											>
+												Adults
+											</label>
+											<div className="field-icon-wrap">
+												<div className="icon">
+													<span className="ion-ios-arrow-down" />
+												</div>
+												<select name="adult" id="adults" className="form-control" onChange={handleChange} value={adult || ''}>
+													<option value="">adult</option>
+													<option value="1">1</option>
+													<option value="2">2</option>
+													<option value="3">3</option>
+													<option value="4">4+</option>
+												</select>
+											</div>
+										</div>
+										<div className="col-md-6 mb-3 mb-md-0">
+											<label
+												htmlFor="children"
+												className="font-weight-bold text-black"
+											>
+												Children
+											</label>
+											<div className="field-icon-wrap">
+
+												<div className="icon">
+													<span className="ion-ios-arrow-down" />
+												</div>
+												<select name="children" id="children" className="form-control" onChange={handleChange} value={children || ''}>
+													<option value="">children</option>
+													<option value="1">1</option>
+													<option value="2">2</option>
+													<option value="3">3</option>
+													<option value="4">4+</option>
+												</select>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div className="col-md-6 col-lg-3 align-self-end" >
+									<button className="btn btn-primary btn-block text-white style-button-home" onClick={Show}>
+										Check Availabilty
+									</button>
+								</div>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</section>
+	)
 }
