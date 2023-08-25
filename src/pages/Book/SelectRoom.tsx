ba806045b4a10f4d9461cd0ea112b60ac619df9f
ThanhThuyPNV <buithithanhthuy08022002@gmail.com>
2023-08-25 10:34:51 +0700

    update filter price

--- src/pages/Book/SelectRoom.tsx
+++ src/pages/Book/SelectRoom.tsx
@@ -1,88 +1,64 @@
-import React, { Component } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBed, faPerson } from '@fortawesome/free-solid-svg-icons';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './SelectRoom.css';
-import axios from 'axios';
+import '../Book/SelectRoom.css'
 
-interface RoomData {
-    id: number;
+type Room = {
+    room_id: string;
     roomname: string;
-    price: string;
-    people: string;
     size: string;
-    images: string;
+    people: number;
+    image: string;
+    price: number;
+};
 
-}
+type SelectRoomProps = {
+    filteredData: Room[];
+};
 
-interface SelectRoomState {
-    roomsData: RoomData[];
-}
-
-export default class SelectRoom extends Component<{}, SelectRoomState> {
-    constructor(props: {}) {
-        super(props);
-        this.state = {
-            roomsData: [],
-        };
-    }
-
-    componentDidMount() {
-        axios.get<RoomData[]>('http://localhost:3000/api/room')  // Đổi địa chỉ API tại đây
-            .then(response => {
-                this.setState({ roomsData: response.data });
-            })
-            .catch(error => {
-                console.error('Error fetching data:', error);
-            });
-    }
-    
-
-    render() {
-        const { roomsData } = this.state;
-
-        return (
-            <div>
-                <div className="container">
-    {roomsData.map(room => (
-        <div key={room.id} className='select-room p-2' data-aos="fade-up">
-            <div className="row booking-room">
-                <div className="col-lg-4 col-sm-12 d-flex justify-content-center">
-                    <img
-                        className="imgroom"
-                        src={room.images}
-                        alt=""
-                        height={'100%'}
-                        width={'100%'}
-                    />
-                </div>
-                <div className="col-lg-4 col-sm-6 d-flex justify-content-center pt-2">
-                    <div>
-                        <h4 style={{ fontFamily: 'Segoe UI' }}>{room.roomname}</h4>
-                        <div className='pt-3 icons'>
-                            <p className='icon-bed'>
-                                <FontAwesomeIcon icon={faBed} /> <p className='bed-room-booking'>{room.size}</p>
-                            </p>
-                            <p className='icon-person'>
-                                <FontAwesomeIcon icon={faPerson} /> <p className='person-room-booking'>{room.people}</p>
-                            </p>
+export default function SelectRoom({ filteredData }: SelectRoomProps) {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-12">
+                    {filteredData.map((room: { room_id: React.Key | null | undefined; image: string | undefined; roomname: boolean | React.ReactChild | React.ReactFragment | React.ReactPortal | null | undefined; size: boolean | React.ReactChild | React.ReactFragment | React.ReactPortal | null | undefined; people: boolean | React.ReactChild | React.ReactFragment | React.ReactPortal | null | undefined; price: boolean | React.ReactChild | React.ReactFragment | React.ReactPortal | null | undefined; }) => (
+                        <div className='select-room p-2' data-aos="fade-up" key={room.room_id}>
+                            <div className="row booking-room">
+                                <div className="col-lg-4 col-sm-12 d-flex justify-content-center">
+                                    <img
+                                        className="imgroom"
+                                        src={room.image} // Assuming the API provides an image URL
+                                        alt=""
+                                        height={'100%'}
+                                        width={'100%'}
+                                    />
+                                </div>
+                                <div className="col-lg-4 col-sm-6 d-flex justify-content-center pt-2">
+                                    <div>
+                                        <h4 style={{ fontFamily: 'Segoe UI' }}>{room.roomname}</h4>
+                                        <div className='pt-3 icons'>
+                                            <p className='icon-bed'>
+                                                <FontAwesomeIcon icon={faBed} /> <p className='bed-room-booking'>{room.size}</p>
+                                            </p>
+                                            <p className='icon-person'>
+                                                <FontAwesomeIcon icon={faPerson} /> <p className='person-room-booking'>{room.people} Adults</p>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-lg-4 col-sm-6 d-flex justify-content-center line pt-2">
+                                    <div className="pice-room">
+                                        <h2 className="pice ">{room.price}đ</h2>
+                                        <button type="button" className="btn btn-primary text-btn-bookingroom">Select</button><br />
+                                        <p className="inforoom">Only Rooms Left !</p>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
-                <div className="col-lg-4 col-sm-6 d-flex justify-content-center line pt-2">
-                    <div className="pice-room">
-                        <h2 className="pice ">{room.price}</h2>
-                        <button type="button" className="btn btn-primary text-btn-bookingroom">Select</button><br />
-                        <p className="inforoom">Only Rooms Left !</p>
-                    </div>
+                    ))}
                 </div>
             </div>
         </div>
-    ))}
-</div>
+    );
+};
 
-            </div>
-        );
-    }
-}
--- src/pages/Book/index.tsx
+++ src/pages/Book/index.tsx
@@ -17,14 +17,65 @@ import Session1 from '../Home/Session1';
 import './index.css';
 import SlRoom from './SlRoom';
 import Sums from './Sums';
+import Filter from './Filter';
 
 
 export default function Booking() {
+    const [roomData, setRoomData] = useState<Room[]>([]);
+    const [filteredData, setFilteredData] = useState<Room[]>([]);
+
+    type Room = {
+        room_id: string;
+        roomname: string;
+        size: string;
+        people: number;
+        image: string;
+        price: number;
+    };
+
+    useEffect(() => {
+        fetch('https://63a572152a73744b008e2940.mockapi.io/api/room')
+            .then(response => response.json())
+            .then(data => {
+                setRoomData(data);
+                setFilteredData(data); 
+            })
+            .catch(error => {
+                console.error('Error fetching room data:', error);
+            });
+
+        AOS.init();
+    }, []);
+    const handleFilter = (priceRange: string) => {
+        const filtered = roomData.filter(room => {
+            const roomPrice = room.price;
+            switch (priceRange) {
+                case 'lessThan500k':
+                    return roomPrice < 500000;
+                case '500kTo1m':
+                    return roomPrice >= 500000 && roomPrice <= 1000000;
+                case '1mTo2m':
+                    return roomPrice > 1000000 && roomPrice <= 2000000;
+                case '2mTo5m':
+                    return roomPrice > 2000000 && roomPrice <= 5000000;
+                case 'greaterThan5m':
+                    return roomPrice > 5000000;
+                default:
+                    return true; 
+            }
+        });
+
+        setFilteredData(filtered);
+    };
+    const handleReset = () => {
+        setSelectedPriceRange(''); 
+        handleFilter(''); 
+    };
     useEffect(() => {
         AOS.init();
     }, []);
 
-    const handleSlRoomSelect = (roomInfo:string, price:string) => {
+    const handleSlRoomSelect = (roomInfo: string, price: string) => {
         const newSelectedRoom = {
             roomInfo: roomInfo,
             price: price
@@ -33,21 +84,21 @@ export default function Booking() {
     return (
         <>
             <Header />
-            <Session1/>
+            <Session1 />
             <div className='container book' data-aos="fade-up">
-            <div className="row ">
-                <Fillter_radio/>
-            </div>
+                <div className="row ">
+                    <Fillter_radio />
+                </div>
                 <div className="row book-room">
-                    <div className="col-lg-10">
-                        <Fillter />
+                    <div className="col-lg-3">
+                        <Filter handleFilter={handleFilter} handleReset={handleReset} />
                     </div>
-                    {/* <div className="col-lg-6">
-                        <SelectRoom />
-                    </div> */}
+                    <div className="col-lg-7">
+                        <SelectRoom filteredData={filteredData} />
+                    </div>
                     <div className="col-lg-2">
                         {/* <Sums /> */}
-                        <Sum/>
+                        <Sum />
                     </div>
                 </div>
             </div><br /><br />
@@ -55,3 +106,7 @@ export default function Booking() {
         </>
     )
 }
+function setSelectedPriceRange(arg0: string) {
+    throw new Error('Function not implemented.');
+}
+
